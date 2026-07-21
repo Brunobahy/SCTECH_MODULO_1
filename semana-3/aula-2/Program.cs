c8ef60392027dbe6553fa27b498d5a0886ede4ca
Brunobahy <bruno.bahy.oliveira@gmail.com>
2026-07-21 20:38:49 -0300

    Adicionando Semana 4

--- semana-3/aula-2/Program.cs
+++ semana-3/aula-2/Program.cs
@@ -1,87 +1,121 @@
-﻿//ATIVIDADE 1 
-string user = "tester";
-string senha = "1234";
-bool logado = false;
-Console.WriteLine("Bem vindo, faça seu login: ");
-for(int i = 1; i <=3; i++)
-{
-    Console.Write("Usuario: ");
-    string userTentativa = Console.ReadLine();
-    Console.Write("Senha: ");
-    string senhaTentativa = Console.ReadLine();
-    if(user == userTentativa && senha == senhaTentativa)
-    {
-        Console.WriteLine("Você esta logado ;)");
-        break;
-    }
-    else
-    {
-        Console.WriteLine("usuario ou senha incorreto");
-        Console.WriteLine($"Você ainda tem {3-i} tentativas");
-    }
-};
-if (logado)
-{
-Console.WriteLine("Você esgotou suas tentativas :(");
-    
-}
+﻿// //ATIVIDADE 1 
+// string user = "tester";
+// string senha = "1234";
+// bool logado = false;
+
+// Console.WriteLine("Bem vindo, faça seu login: ");
+// for (int i = 1; i <= 3; i++)
+// {
+//     Console.Write("Usuario: ");
+//     string userTentativa = Console.ReadLine();
+//     Console.Write("Senha: ");
+//     string senhaTentativa = Console.ReadLine();
+//     if (user == userTentativa && senha == senhaTentativa)
+//     {
+//         Console.WriteLine("Você esta logado ;)");
+//         break;
+//     }
+//     else
+//     {
+//         Console.WriteLine("usuario ou senha incorreto");
+//         Console.WriteLine($"Você ainda tem {3 - i} tentativas");
+//     }
+// }
+// ;
+// if (logado)
+// {
+//     Console.WriteLine("Você esgotou suas tentativas :(");
+
+// }
 
 //ATIVIDADE 2
 
 
-bool sair = false;
-float saldo=0;
+// bool sair = false;
+// float saldo = 0;
 
-do
+// do
+// {
+//     Console.WriteLine("***************");
+//     Console.WriteLine("1 - Depositar");
+//     Console.WriteLine("2 - Sacar");
+//     Console.WriteLine("3 - Ver Saldo");
+//     Console.WriteLine("4 - sair");
+//     bool validador = int.TryParse(Console.ReadLine(), out int resposta);
+//     if (validador)
+//     {
+//         switch (resposta)
+//         {
+//             case 1:
+//                 Console.WriteLine("Quanto você quer depositar?");
+//                 Console.Write("R$");
+//                 bool vValor = float.TryParse(Console.ReadLine(), out float valor);
+//                 if (vValor && valor > 0)
+//                 {
+//                     saldo += valor;
+//                 }
+//                 else
+//                 {
+//                     Console.WriteLine("Valor Invalido");
+//                 }
+//                 ;
+//                 continue;
+//             case 2:
+//                 Console.WriteLine("Quanto você quer sacar?");
+//                 Console.Write("R$");
+//                 vValor = float.TryParse(Console.ReadLine(), out valor);
+//                 if (vValor && valor > 0 && valor <= saldo)
+//                 {
+//                     saldo -= valor;
+//                 }
+//                 else
+//                 {
+//                     Console.WriteLine("Valor Invalido ou saldo insuficiente");
+//                 }
+//                 ;
+//                 continue;
+//             case 3:
+//                 Console.WriteLine($"Seu saldo é R${saldo}");
+//                 continue;
+//             case 4:
+//                 Console.WriteLine("Você esta saindo, tchau ;)");
+//                 sair = true;
+//                 continue;
+//             default:
+//                 continue;
+
+//         }
+//     }
+// } while (!sair);
+
+
+
+
+int SomaDoisNumeros(int num1, int num2)
 {
-    Console.WriteLine("***************");
-    Console.WriteLine("1 - Depositar");
-    Console.WriteLine("2 - Sacar");
-    Console.WriteLine("3 - Ver Saldo");
-    Console.WriteLine("4 - sair");
-    bool validador = int.TryParse(Console.ReadLine(),out int resposta);
-    if (validador)
-    {
-        switch(resposta){
-            case 1:
-                Console.WriteLine("Quanto você quer depositar?");
-                Console.Write("R$");
-                bool vValor = float.TryParse(Console.ReadLine(),out float valor);
-                if (vValor && valor >0)
-                {
-                    saldo+=valor;
-                }
-                else
-                {
-                    Console.WriteLine("Valor Invalido");
-                }
-                ;
-                continue;
-            case 2:
-                Console.WriteLine("Quanto você quer sacar?");
-                Console.Write("R$");
-                vValor = float.TryParse(Console.ReadLine(),out valor);
-                if (vValor && valor >0 && valor<=saldo)
-                {
-                    saldo-=valor;
-                }
-                else
-                {
-                    Console.WriteLine("Valor Invalido ou saldo insuficiente");
-                }
-                ;
-                continue;
-            case 3:
-                Console.WriteLine($"Seu saldo é R${saldo}");
-                continue;
-            case 4:
-                Console.WriteLine("Você esta saindo, tchau ;)");
-                sair = true;
-                continue;
-            default:
-                continue;
-            
+    int soma = num1+num2;
+    Console.WriteLine($"{num1} + {num2} = {soma}");
+    return soma;
+}
+
+
+
+int obtemNumero()
+{   
+    int numero;
+    while (true){
+        Console.WriteLine("Digite um numero:");
+        bool valido = int.TryParse(Console.ReadLine(), out numero);
+        if (!valido)
+        {
+            Console.WriteLine("Digite um valor valido!");
+        }
+        else
+        {
+            break;
         }
     }
-}while(!sair);
+    return numero;
+}
 
+SomaDoisNumeros(obtemNumero(),obtemNumero());
---
+++ semana-4/aula-1/Program.cs
@@ -0,0 +1,84 @@
+﻿
+// // int[] arrayNum = new int[3];
+// // var arrayNumComVar = new int[3];
+
+// // int num1;
+// // num1 = 5;
+// // Console.WriteLine(num1);
+
+// // int[] arrayNum = new int[4];
+
+// // arrayNum[0] = 5;
+// // Console.WriteLine(arrayNum[0]);
+// // arrayNum[1] = 6;
+// // Console.WriteLine(arrayNum[1]);
+// // arrayNum[2] = 7;
+// // Console.WriteLine(arrayNum[2]);
+// // arrayNum[3] = 8;
+// // Console.WriteLine(arrayNum[3]);
+
+// // string[] arrayTexto = new string[3];
+
+// // arrayTexto[0] = "Bruno";
+// // Console.WriteLine(arrayTexto[0]);
+// // arrayTexto[1] = "Gislaine";
+// // Console.WriteLine(arrayTexto[1]);
+// // arrayTexto[2] = "Astolfo";
+// // Console.WriteLine(arrayTexto[2]);
+
+// bool validaTm = false;
+// int tamanho = 0;
+// do{ 
+//     Console.Write("Digite o tamanho do array: ");
+//     validaTm = int.TryParse(Console.ReadLine(), out tamanho);
+
+// }while(!validaTm);
+
+// string[] array = new string[tamanho];
+
+// for(var i=0; i<tamanho; i++)
+// {
+//     Console.Write($"Digite o {i+1}° nome na posição: ");
+//     array[i]=Console.ReadLine();
+// }
+
+// for (int i = 0; i < tamanho; i++)
+// {
+//     Console.WriteLine($"Nome {i+1}: {array[i]}");
+// }
+
+/// Crie um programa para calculo de média que pegunte quantas notas,
+/// obtenha os valores delas,  e  realize  o calculo e depois exiba a nota.
+int tamanho = 0;
+bool validaTm = false;
+
+Console.WriteLine("Bem vindo a calculadora de media");
+do
+{
+    Console.Write("Quantas notas devo calcular? ");
+    validaTm = int.TryParse(Console.ReadLine(),out tamanho);
+}while(!validaTm && tamanho <=0);
+
+float[] notas = new float[tamanho];
+float soma = 0; 
+for(int i = 0; i < tamanho; i++)
+{
+    bool validaNota = false;
+    float atual;
+    do{
+        Console.Write($"Digite a {i+1}° nota: ");
+        validaNota = float.TryParse(Console.ReadLine(),out atual);
+    }while(!validaNota);
+
+    notas[i]= atual;
+    soma += atual;
+}
+float media = soma/tamanho;
+
+Console.WriteLine("");
+Console.WriteLine($"A media das {tamanho} notas é: {media}");
+Console.WriteLine("Notas fornecidas: ");
+for(int i = 0; i < tamanho; i++)
+{
+    Console.WriteLine($"{i+1}) {notas[i]}");
+};
